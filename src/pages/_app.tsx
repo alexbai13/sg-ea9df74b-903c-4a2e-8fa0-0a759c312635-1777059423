78321ca34685b52798ecc748025196f4b66e1c22
softgenai <admin@softgen.ai>
2026-05-18 19:01:12 +0000

    feat(ui): add theme provider to app wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,19 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CookieBanner } from "@/components/CookieBanner";
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/contexts/ThemeProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
       <Component {...pageProps} />
       <CookieBanner />
       <Toaster />
-    </>
+    </ThemeProvider>
   );
 }
