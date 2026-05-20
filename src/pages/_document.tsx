e3dd4bd5134e810187d92d1e3fea96233a2b75fe
Softgen Dev <dev@softgen.ai>
2026-05-20 01:09:18 +0000

    feat(tracking): add hubspot script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,6 +38,10 @@ export default function Document() {
             data-softgen-visual-editor="true"
           />
         )}
+
+        {/* Start of HubSpot Embed Code */}
+        <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/51492519.js"></script>
+        {/* End of HubSpot Embed Code */}
       </body>
     </Html>
   );
